f7413432a5488de9f8fa992ffe8aaf2e7cf27b10
Pablo Sánchez Pérez <psp59@alu.ua.es>
2023-03-17 18:32:53 +0100

    Añadido movimientos y fisicas del personaje

---
+++ Morning Kebab/Assets/Sprites/Scripts/kebabCollected.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kebab : MonoBehaviour
+{
+    public static bool isKebab;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+    }
+}
