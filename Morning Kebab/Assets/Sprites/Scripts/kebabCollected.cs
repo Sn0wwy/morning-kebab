f9c7ba7ae6776a25920e4fa4e800ae27b82a4aa0
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-04-02 16:13:44 +0200

    Añadido protagonista

--- Morning Kebab/Assets/Sprites/Scripts/kebabCollected.cs
+++ Morning Kebab/Assets/Sprites/Scripts/kebabCollected.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class kebab : MonoBehaviour
+public class kebabCollected : MonoBehaviour
 {
     public static bool isKebab;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Destroy(gameObject, 0.5f);
+        }
     }
 }
