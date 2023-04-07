595e452daf4fe86ebbb0c1d81ad31483a9654835
Ignacio Alavés <63061407+Sn0wwy@users.noreply.github.com>
2023-04-07 19:49:08 +0200

    Añadido funcionalidad de despawnear las balas

--- Morning Kebab/Assets/Scripts/CoordinatedBulletScript.cs
+++ Morning Kebab/Assets/Scripts/CoordinatedBulletScript.cs
@@ -7,6 +7,7 @@ public class CoordinatedBulletScript : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     public float force;
+    private float timer;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,17 @@ public class CoordinatedBulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > 2){
+            Destroy(gameObject);
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            Destroy(gameObject);
+        }
+    }
+
 }
--- Morning Kebab/Assets/Scripts/EnemyShoot.cs
+++ Morning Kebab/Assets/Scripts/EnemyShoot.cs
@@ -8,20 +8,25 @@ public class EnemyShoot : MonoBehaviour
     public Transform bulletPos;
 
     private float timer;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if(timer > 2){
-            timer = 0;
-            shoot();
+        if(distance < 3.5) {
+            timer += Time.deltaTime;
+
+            if(timer > 2){
+                timer = 0;
+                shoot();
+            }
         } 
     }
 
