ae9607694d6b90eb684a34719558ad33c66682aa
Ignacio Alavés <63061407+Sn0wwy@users.noreply.github.com>
2023-04-10 14:07:59 +0200

    Añadido sistema de vidas

---
+++ Morning Kebab/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+
+    private void Awake() {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage){
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if(currentHealth > 0) {
+            // Player hurt
+
+        } else {
+            // Player dead
+        }
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.E)) {
+            TakeDamage(1);
+        }
+
+    }
+}
