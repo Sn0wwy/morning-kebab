13680635b1096dfe8785962ec2b64bb1b11a3d68
Álvaro <als106@alu.ua.es>
2023-04-12 15:10:36 +0200

    Create animations and camera movement

---
+++ Morning Kebab/Assets/Scripts/JohnMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class JohnMovement : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D Rigidbody2D;
+    private float horizontal;
+    private bool Grounded;
+    private Animator Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
+        }
+        else if (horizontal > 0.0f) {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        Animator.SetBool("running", horizontal != 0.0f);
+        if (Input.GetKeyDown(KeyCode.W) && Grounded)
+        {
+            Jump();
+        }
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else { Grounded = false; }
+    }
+    
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(horizontal * Speed, Rigidbody2D.velocity.y);
+    }
+}
