eb9eb9615c177c06c81b3cf5228c80a2b061b85b
AaronGarcia <agv85@alu.ua.es>
2023-05-19 17:44:26 +0200

    Añadidos menús

---
+++ Morning Kebab/Assets/MENUINICIAL.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENUINICIAL : MonoBehaviour
+{
+    public void Jugar() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir...");
+        Application.Quit();
+    }
+}
