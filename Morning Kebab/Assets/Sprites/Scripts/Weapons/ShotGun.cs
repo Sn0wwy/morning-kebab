7e89dd185bb755fa936afd8292ff336ff60eb6ab
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-05-23 14:06:46 +0200

    Arreglado armas

--- Morning Kebab/Assets/Sprites/Scripts/Weapons/ShotGun.cs
+++ Morning Kebab/Assets/Sprites/Scripts/Weapons/ShotGun.cs
@@ -46,7 +46,7 @@ public class ShotGun : GunWeapon
 
     IEnumerator WaitAndDestroy(GameObject bullet)
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0.5f);
         Destroy(bullet);
     }
 
