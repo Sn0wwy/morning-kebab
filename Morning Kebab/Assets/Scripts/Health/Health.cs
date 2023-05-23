b6e66720b976242953094db16306a5d0b109a2cc
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-05-24 00:04:03 +0200

    Añadido fin de juego y menú pausa a mundo 2

--- Morning Kebab/Assets/Scripts/Health/Health.cs
+++ Morning Kebab/Assets/Scripts/Health/Health.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -34,6 +35,9 @@ public class Health : MonoBehaviour
             if (!dead) {
                 Destroy(gameObject);
                 dead = true;
+                if ( gameObject.tag == "Boss" ){
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+                }
             }
         }
     }
