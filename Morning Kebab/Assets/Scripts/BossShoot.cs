c99d07c00c6d66a7d847044ad08bffaf4550d348
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-05-23 13:47:42 +0200

    Hecho nivel jefe final

---
+++ Morning Kebab/Assets/Scripts/BossShoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    public float shootDelay = 2;
+    public float playerDistance = 3;
+
+    private float timer;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if(distance < playerDistance) {
+            timer += Time.deltaTime;
+
+            if(timer > shootDelay){
+                timer = 0;
+                shoot();
+            }
+        } 
+    }
+
+    void shoot() {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
